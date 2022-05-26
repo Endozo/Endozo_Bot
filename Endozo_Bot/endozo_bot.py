c6b197cf5f4c563785b48659b92ad89b0b1063b1
B1029007 <90619554+Endozo@users.noreply.github.com>
2022-05-26 16:09:10 +0800

    Endozo_Bot_third

--- Endozo_Bot/endozo_bot.py
+++ Endozo_Bot/endozo_bot.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+import json
+import random
+
+with open('setting.json', 'r', encoding='utf8') as jfile:
+    jdata = json.load(jfile)
 
 bot = commands.Bot(command_prefix= '!')
 
@@ -10,17 +15,27 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     print(f'{member} join!')
-    channel = bot.get_channel(978648503674867712)
+    channel = bot.get_channel(int(jdata['Welcome_channel']))
     await channel.send(f"{member} 從天而降!")
 
 @bot.event
 async def on_member_remove(member):
     print(f'{member} remove!')
-    channel = bot.get_channel(978648525812428872)
+    channel = bot.get_channel(int(jdata['Leave_channel']))
     await channel.send(f"{member} 掉頭不回!")
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f"{round(bot.latency*1000)}(ms)")
 
-bot.run("OTc4NjE4MjgyMzMwNDM5Njkw.GtaXmA.K32HY2DdesHKkExEFMeCDFA5nRk65e_hp3Z_0o")
+@bot.command()
+async def pic(ctx):
+    random_pic = random.choice(jdata['pic'])
+    picture = discord.File(random_pic)
+    await ctx.send(file = picture)
+
+@bot.command()
+async def yes(ctx):
+    await ctx.send("yeah baby!")
+
+bot.run(jdata['TOKEN'])
